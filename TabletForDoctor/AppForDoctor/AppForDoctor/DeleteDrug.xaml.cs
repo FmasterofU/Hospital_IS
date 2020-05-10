b4e894d2444dfb95dc8fe6d1333298f233db98ac
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2020-05-11 00:10:19 +0200

    ListBox vs ComboBox (drugLists)

---
+++ TabletForDoctor/AppForDoctor/AppForDoctor/DeleteDrug.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppForDoctor
+{
+    /// <summary>
+    /// Interaction logic for DeleteDrug.xaml
+    /// </summary>
+    public partial class DeleteDrug : Window
+    {
+        public DeleteDrug()
+        {
+            InitializeComponent();
+            List<String> drugList = DrugsPage.getDrugList();
+            for(int i = 0; i < drugList.Count; i++) deleteDrugsComboBox.Items.Add(drugList[i]);
+            deleteDrugsComboBox.SelectedIndex = 0;
+        }
+
+        private void DeleteDrugWindow_Closed(object sender, EventArgs e)
+        {
+            this.Close();
+            DrugsPage.closeDeletion();
+        }
+
+        private void deleteDrugButton_Click(object sender, RoutedEventArgs e)
+        {
+            int index = deleteDrugsComboBox.SelectedIndex;
+            deleteDrugsComboBox.Items.RemoveAt(index);
+            DrugsPage d = DrugsPage.getInstance();
+            d.deleteDrugFormList(index);
+            deleteDrugsComboBox.SelectedIndex = 0;
+            if (deleteDrugsComboBox.Items.Count == 0)   this.Close();
+        }
+    }
+}
--- TabletForDoctor/AppForDoctor/AppForDoctor/DrugsPage.xaml.cs
+++ TabletForDoctor/AppForDoctor/AppForDoctor/DrugsPage.xaml.cs
@@ -20,9 +20,43 @@ namespace AppForDoctor
     /// </summary>
     public partial class DrugsPage : Page
     {
+        private static DeleteDrug delete = null;
+        private static List<String> drugList = new List<String>();
+        private static DrugsPage instance = null;
         public DrugsPage()
         {
             InitializeComponent();
+            //TODO: load drugs from database
+            drugListBox.Items.Add("Prvi");
+            drugListBox.Items.Add("Drugi");
+            drugListBox.Items.Add("Treci");
+            drugListBox.Items.Add("Cetvrti");
+            for (int i = 0; i < drugListBox.Items.Count; i++)
+            {
+                drugList.Add(drugListBox.Items[i].ToString());
+            }
+            instance = this;
+        }
+
+        public static DrugsPage getInstance()
+        {
+            return instance;
+        }
+
+        public void deleteDrugFormList(int index)
+        {
+            drugList.RemoveAt(index);
+            drugListBox.Items.RemoveAt(index);
+        }
+
+        public static List<String> getDrugList()
+        {
+            return drugList;
+        }
+
+        public static void closeDeletion()
+        {
+            delete = null;
         }
 
         private void examinationFromDrugsButton_Click(object sender, RoutedEventArgs e)
@@ -34,6 +68,17 @@ namespace AppForDoctor
         private void drugListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             //TODO: implement selection
+        }
+
+        private void deleteDrugButton_Click(object sender, RoutedEventArgs e)
+        {
+            delete = new DeleteDrug();
+            delete.ShowDialog();
+        }
+
+        private void addDrugButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: implement adding new drug to patient
         }
     }
 }
