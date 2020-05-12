2340c2b7054276223422ff6a8a4ccdfa3bd52030
BozidarArsic <40611211+BoKna031@users.noreply.github.com>
2020-05-12 22:18:14 +0200

    Sekretarsko cudo

---
+++ HCIProjekat/HCIProjekat/Pages/LogPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCIProjekat.Pages
+{
+    /// <summary>
+    /// Interaction logic for LogPage.xaml
+    /// </summary>
+    public partial class LogPage : Page
+    {
+        public LogPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClickPrijaviSe(object sender, RoutedEventArgs e)
+        {
+            UserControls.LoginUC lguc = new UserControls.LoginUC();
+            LoginView.Children.Clear();
+            LoginView.Children.Add(lguc);
+            
+        }
+    }
+}
