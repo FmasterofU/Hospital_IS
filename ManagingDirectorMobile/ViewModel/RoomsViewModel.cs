ffccdf3ff1a85fdd6a5ecae5c087a45fe6d89ec6
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-06-29 01:48:19 +0200

    qrac

--- ManagingDirectorMobile/ViewModel/RoomsViewModel.cs
+++ ManagingDirectorMobile/ViewModel/RoomsViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Controller;
+using Model.Rooms;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,11 @@ namespace ManagingDirectorMobile.ViewModel
 {
     class RoomsViewModel
     {
-
+        private static Controller.IDrugController c = new DrugController();
+        public ObservableCollection<Room> rooms { get; set; }
+        public RoomsViewModel()
+        {
+            
+        }
     }
 }
